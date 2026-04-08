2989750de64883a046fb2f3d2d83b890b1ddd003
zestyplace <selinakim26@gmail.com>
2026-04-09 00:22:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/iljin-app/',
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
-  }
+  base: '/-Daily-Fortune-/',
 })
